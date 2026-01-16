69934313b37ecf3c0fab9a9f89aa1411d084932e
Your Name <you@example.com>
2026-01-16 14:26:50 +0800

    Update: Rename website.html to index.html, update navigation links, update Botpress workflow and add session ID

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
+// Generate a unique ID for this session
+const sessionId = 'session-' + Math.random().toString(36).substr(2, 9);
+
 // Initialize Botpress Webchat
 window.botpress.init({
-    //"botId": "7a95fc67-b765-4583-8794-2d85c6dfffa1", //
-    "clientId": "a6f2c39d-2221-4d0a-92a3-48cfb93078c0", //
-    "selector": "#webchat", //
+    "botId": "ec45131a-f5a7-4051-a15e-d2b05da579fb",
+    "clientId": "f14e1bf1-e0c4-4f8d-9ef3-8ee2efeadd0b",
+    "userId": sessionId, // Force a reset on refresh
+    "selector": "#webchat",
     "configuration": {
-        "botName": "SpeakEasy Korean", //
-        "botDescription": "Your AI Korean Language Tutor", //
-        
-        /* PASTE YOUR AVATAR IMAGE URL BELOW */
-        "botAvatar": "https://img.freepik.com/premium-vector/south-korea-flag-heart-shape_493206-528.jpg", 
-        
-        "color": "#f97316", // SpeakEasy Orange
+        "version": "v2",
+        "composerPlaceholder": "type your message here..",
+        "botName": "SpeakEasy Korean Assistant",
+        "botDescription": "SpeakEasy Korean is an interactive AI tutor that helps learners practise Korean through quick drills, roleplay scenarios, and focused lessons.\nThe chatbot adapts its teaching style and content based on the learner's level, goals, and interests, making Korean learning simple, engaging, and practical.",
+        "botAvatar": "https://files.bpcontent.cloud/2026/01/16/04/20260116045502-PNL50NEI.png",
+        "color": "#90d6e3",
         "variant": "solid",
+        "headerVariant": "solid",
         "themeMode": "light",
-        "fontFamily": "inter",
-        "radius": 1.5
+        "fontFamily": "ADLaM Display",
+        "radius": 3,
+        "feedbackEnabled": false,
+        "soundEnabled": false
     }
 });
 
